f133ce4dea33aa18cd2b0bae18127e384a836f6c
tebr1z <tabrizyh@code.edu.az>
2023-06-06 18:44:17 +0400

    add

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ window.addEventListener('scroll', function () {
   if (scrollTop > 0)
   
   
-  //Added //js //jquer //jue
+  //Added //js //jquer //jue //vue
   {
     navbar.classList.add('fixed');
     navbarLinks.classList.add('navbar-links-color')
